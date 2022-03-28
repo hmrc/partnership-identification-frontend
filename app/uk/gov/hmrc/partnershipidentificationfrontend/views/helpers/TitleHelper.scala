99a0f23ab8b703bc30085929cda642d589defc15
tvaidotas <1133367+tvaidotas@users.noreply.github.com>
2022-03-28 17:57:31 +0100

    updated the TitleHelper

--- app/uk/gov/hmrc/partnershipidentificationfrontend/views/helpers/TitleHelper.scala
+++ app/uk/gov/hmrc/partnershipidentificationfrontend/views/helpers/TitleHelper.scala
@@ -18,9 +18,20 @@ package uk.gov.hmrc.partnershipidentificationfrontend.views.helpers
 
 import play.api.data.Form
 import play.api.i18n.Messages
+import uk.gov.hmrc.partnershipidentificationfrontend.config.AppConfig
+import uk.gov.hmrc.partnershipidentificationfrontend.models.PageConfig
 
 object TitleHelper {
-  def title(titleMessage: String, form: Form[_])(implicit messages: Messages): String =
+  def title(titleKey: String, pageConfig: PageConfig, form: Form[_])(implicit messages: Messages, appConfig: AppConfig): String = {
+    val titleMessage: String = s"${messages(titleKey)} - ${getServiceName(pageConfig)} - ${messages("service.govuk")}"
     if (form.hasErrors) messages("error.title-prefix") + titleMessage
     else titleMessage
+  }
+  def title(titleKey: String, pageConfig: PageConfig)(implicit messages: Messages, appConfig: AppConfig): String = {
+    s"${messages(titleKey)} - ${getServiceName(pageConfig)} - ${messages("service.govuk")}"
+  }
+
+  private def getServiceName(pageConfig: PageConfig)(implicit messages: Messages, appConfig: AppConfig) =
+    pageConfig.optServiceName.getOrElse(appConfig.defaultServiceName)
+
 }
